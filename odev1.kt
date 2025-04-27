a646e0e81bd0a4b143f7287e10761df408cdff7b
aliemreyr0 <yaraliemre0@gmail.com>
2025-04-27 15:37:07 +0300

    Add files via upload

---
+++ odev1.kt
@@ -0,0 +1,92 @@
+var sehir = "İstanbul"
+println(sehir)
+val ulke = "Türkiye"
+println(ulke)
+var telefon = "05551234567"
+println(telefon)
+val postaKodu = "34000"
+println(postaKodu)
+var email = "ornek@mail.com"
+println(email)
+val meslek = "Yazilimci"
+println(meslek)
+var stokMiktari = 150
+println(stokMiktari)
+val musteriAdi = "Ahmet Yılmaz"
+println(musteriAdi)
+var bakiye = 1250.75
+println(bakiye)
+val dogumGunu = "1995-08-21"
+println(dogumGunu)
+var maas = 25000
+println(maas)
+val medeniDurum = "Bekar"
+println(medeniDurum)
+var urunYorum = "Harika bir ürün!"
+println(urunYorum)
+val odemeTarihi = "2025-05-01"
+println(odemeTarihi)
+var odeme = 999.99
+println(odeme)
+val siparisAdeti = 3
+println(siparisAdeti)
+var arabaModeli = "Toyota Corolla"
+println(arabaModeli)
+val kitapAdi = "Kotlin Rehberi"
+println(kitapAdi)
+var yayinlamaTarihi = "2023-10-15"
+println(yayinlamaTarihi)
+val indirimMiktari = 15
+println(indirimMiktari)
+var odaSayisi = 2
+println(odaSayisi)
+val enlem = 41.0082
+println(enlem)
+var boylam = 28.9784
+println(boylam)
+val urunAdi = "Bluetooth Kulaklik"
+println(urunAdi)
+var yemekFiyati = 200.0
+println(yemekFiyati)
+val marka = "Samsung"
+println(marka)
+var muzikAdi = "Bir Başka"
+println(muzikAdi)
+val videoSuresi = "3 dakika"
+println(videoSuresi)
+var urunPuani = 4.8
+println(urunPuani)
+val resimAdi = "urun_resmi.jpg"
+println(resimAdi)
+var dosyaFormati = "jpg"
+println(dosyaFormati)
+val renk = "Mavi"
+println(renk)
+var renkKodu = "#0000FF"
+println(renkKodu)
+val telefonModeli = "iPhone 13"
+println(telefonModeli)
+var ekranBoyutu = "6.1 inç"
+println(ekranBoyutu)
+val agirlik = "174 gram"
+println(agirlik)
+var ulusalGun = "23 Nisan"
+println(ulusalGun)
+val tatilGunu = "Cumartesi"
+println(tatilGunu)
+var rezervasyonTarihi = "2025-07-10"
+println(rezervasyonTarihi)
+val sokakAdi = "İstiklal Caddesi"
+println(sokakAdi)
+var otobusHatti = "34AS"
+println(otobusHatti)
+val kalanDakika = 5
+println(kalanDakika)
+var takipKodu = "ABC123XYZ"
+println(takipKodu)
+val kuponSuresi = "30 gün"
+println(kuponSuresi)
+var kuponKodu = "INDIRIM50"
+println(kuponKodu)
+val faturaAdresi = "Ataşehir/İstanbul"
+println(faturaAdresi)
